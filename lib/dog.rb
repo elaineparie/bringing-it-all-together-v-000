15cf75f1aa5bc1b87e7405fe6e8be7ed7d751fae
Elaine Anderson <eparie@bu.edu>
2018-07-03 17:21:58 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -64,6 +64,7 @@ def self.find_by_id(id)
 end
 
 def self.find_or_create_by(hash)
+  binding.pry
   dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
   binding.pry
  if !dog.empty?
