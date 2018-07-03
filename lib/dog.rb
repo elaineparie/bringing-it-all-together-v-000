2589fa214128b8a34e2b07a523c3bf81518f2109
Elaine Anderson <eparie@bu.edu>
2018-07-03 16:10:00 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -25,7 +25,7 @@ def self.drop_table
   end
 
   def self.new_from_db(row)
-  new_dog = self.new(hash)
+  new_dog = self.new(row)
 #  hash[:id] = row[0]
 #  hash[:name] =  row[1]
 #  hash[:breed] = row[2]
