229765f35f74221a5f201823491a4e96e1767b2e
Elaine Anderson <eparie@bu.edu>
2018-07-03 15:23:33 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,9 @@
-class Dog 
-attr_accessor :name, :breed, :id 
+class Dog
+attr_accessor :name, :breed, :id
 
 def initialize(name, breed, id = nil)
-  @name = name 
-  @breed = breed 
+  @name = name
+  @breed = breed
   @id = id
-end 
-end 
+end
+end
