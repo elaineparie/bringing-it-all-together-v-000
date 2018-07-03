8db23841d90506819ea8e4a8bae6446285fcaba4
Elaine Anderson <eparie@bu.edu>
2018-07-03 16:21:49 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -8,6 +8,7 @@ def initialize(dog_hash)
   #@id = dog_hash[:id]
 end
 
+
 def self.create_table
 sql = <<-SQL
 CREATE TABLE IF NOT EXISTS dogs (
@@ -19,17 +20,32 @@ SQL
 DB[:conn].execute(sql)
 end
 
+
 def self.drop_table
     sql = "DROP TABLE IF EXISTS dogs"
     DB[:conn].execute(sql)
   end
 
-  def self.new_from_db(row)
-  new_dog = self.new(hash)
-  new_dog.name =  row[1]
-  new_dog.breed = row[2]
-  new_dog
-  end
+  #def self.new_from_db(row)
+  #new_dog = self.new(dog_hash)
+  #new_dog.name =  row[1]
+  #new_dog.breed = row[2]
+  #new_dog
+  #end
+
+  def save
+  #  if self.id
+  #self.update
+#else
+  sql = <<-SQL
+    INSERT INTO dogs (name, breed)
+    VALUES (?, ?)
+  SQL
+
+  DB[:conn].execute(sql, self.name, self.breed)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+  #end
+end
 
 
 end
