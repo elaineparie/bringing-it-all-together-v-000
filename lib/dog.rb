3d17a62b99923e5e5122b3b15dbb3b4822abc03d
Elaine Anderson <eparie@bu.edu>
2018-07-03 15:51:25 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -7,4 +7,16 @@ def initialize(hash)
   @breed = hash[:breed]
   @id = hash[:id]
 end
+
+def self.create_table
+sql = <<-SQL
+CREATE TABLE IF NOT EXISTS dogs (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  breed TEXT
+)
+SQL
+DB[:conn].execute(sql)
+end
+
 end
